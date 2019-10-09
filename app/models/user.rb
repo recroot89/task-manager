c930ade51e07042be99f0213ceff4c513a893eb9
recroot89 <recroot89@gmail.com>
2019-10-10 03:08:04 +0500

    add user references and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true,
+                    format: {
+                      with: /^(?:[^\s@])+@{1}(?:[^\s@.])+\.(?:[^\s@.])+$/,
+                      message: 'email is incorrect'
+                    }
 end
