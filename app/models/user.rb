a9dca315ddb0b55c08a188bdf096999def5dc2f8
recroot89 <recroot89@gmail.com>
2019-10-20 22:27:57 +0500

    update menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
     with: /\A(?:[^\s@])+@{1}(?:[^\s@.])+\.(?:[^\s@.])+\z/,
     message: 'email is incorrect'
   }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
