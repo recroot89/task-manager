b73955196dc2fd3ad2c6516bb4d2c270bc4f4458
recroot89 <recroot89@gmail.com>
2019-10-20 20:23:40 +0500

    add admin panel authorization

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,4 +3,15 @@
 class Admin::ApplicationController < ApplicationController
   include AuthHelper
   helper_method :current_user
+  before_action :authenticate_user!, :authorize
+
+  def authorize
+    if forbidden?
+      render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false)
+    end
+  end
+
+  def forbidden?
+    !current_user.is_a?(Admin)
+  end
 end
