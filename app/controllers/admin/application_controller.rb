fe698a92dc9ea461750191ac1ad267ef2b895304
recroot89 <recroot89@gmail.com>
2019-10-20 20:14:32 +0500

    add admin users page

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Admin::ApplicationController < ApplicationController
+  include AuthHelper
+  helper_method :current_user
+end
---
+++ test/controllers/admin_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+  test 'should get show' do
+    user = create(:user)
+    get admin_user_url(user)
+    assert_response :success
+  end
+end
