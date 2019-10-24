d4e654d11802aef5931b8cf0b642f2c3b6539606
recroot89 <recroot89@gmail.com>
2019-10-24 18:28:43 +0500

    add task meta and serializer

---
+++ app/serializers/task_serializer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class TaskSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :state, :expired_at
+  belongs_to :author
+  belongs_to :assignee
+
+  def id
+    object.id.to_s
+  end
+end
